c45d7a80c8117ceca6deae84c96cd63d22492e07
Festongithub <fkemboik@gmail.com>
2024-10-18 17:13:13 +0300

    user-interface part one

--- Tubonge_Room/lib/chat_server.js
+++ Tubonge_Room/lib/chat_server.js
@@ -1,9 +1,29 @@
-var socketio =require('socket.io');
+var socketio = require('socket.io');
 var io;
 var guestNumber = 1;
 var nickNames = {};
 var namesUsed = [];
 var currentRoom = {};
+
+exports.listen = function(server) {
+    io = socketio(server);
+    //io.set('log level', 1);
+
+    io.on('connection', function(socket){
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+        joinRoom(socket, 'Lobby');
+
+        handleMessageBroadCasting(socket, nickNames);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+
+        socket.on('rooms', function(){
+            socket.emit('rooms', io.sockets.manager.rooms);
+        });
+
+        handleClientDisconnection(socket, nickNames, namesUsed)
+    });
+};
 
 // Assigning Guest Names
 function assignGuestName(socket, guestNumber, nickNames, namesUsed){
@@ -102,24 +122,3 @@ function handleClientDisconnection(socket) {
         delete nickNames[socket.id];
     });
 }
-// Establishing connection logic
-
-exports.listen = function(server) {
-    io = socket.io.listen(server);
-    io.set('log level', 1);
-
-    io.sockets.on('connection', function(socket){
-        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-        joinRoom(socket, 'Lobby');
-
-        handleMessageBroadCasting(socket, nickNames);
-        handleNameChangeAttempts(socket, nickNames, namesUsed);
-        handleRoomJoining(socket);
-
-        socket.on('rooms', function(){
-            socket.emit('rooms', io.sockets.manager.rooms);
-        });
-
-        handleClientDisconnection(socket, nickNames, namesUsed)
-    });
-};

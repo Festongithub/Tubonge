d311e8c651890cefb388772d8a6d1eb1bb45dad8
Festongithub <fkemboik@gmail.com>
2024-10-26 18:49:16 +0300

    Logout User

--- Chat_Room/client/src/context/AuthContext.jsx
+++ Chat_Room/client/src/context/AuthContext.jsx
@@ -6,22 +6,37 @@ export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [registerError, setRegisterError] = useState(null);
     const [isRegisterLoading,  setIsRegisterLoading] = useState(false);
-
     const [registerInfo, setRegisterInfo] = useState({
         name: "",
         email: "",
         password: "",
 
     });
+
+    // logininfo
+    const [loginError, setLoginError] = useState(null);
+    const [isloginLoading,  setIsLoginLoading] = useState(false);
+    const [loginInfo, setloginInfo] = useState({
+        name: "",
+        email: "",
+    });
+
     console.log(`User`, user);
 
     useEffect(() =>{
         const user = localStorage.getItem("User")
         setUser(JSON.parse(user));
-    }, [])
+    }, []
+    );
+
+    // update userInfo
 
     const updateRegisterInfo = useCallback((info) => {
         setRegisterInfo(info);
+    }, []);
+
+    const updateloginInfo = useCallback((info) => {
+        setloginInfo(info);
     }, []);
 
     const registerUser = useCallback(async(e) =>  {
@@ -44,6 +59,30 @@ export const AuthContextProvider = ({children}) => {
     }, [registerInfo]
     );
 
+    // implement user loginifo
+    const loginUser = useCallback(async(e) => {
+        e.preventDefault()
+
+        setIsLoginLoading(true)
+        setLoginError(null)
+
+        const response = await postRequest(
+            `${baseUrl}/users/login`,
+            JSON.stringify(loginInfo)
+        );
+        setIsLoginLoading(false);
+
+        //handle erro
+
+        if(response.error){
+            return setLoginError(response)
+        }
+        localStorage.setItem("User", JSON.stringify(response))
+        setUser(response);
+
+    }, [loginInfo]);
+
+
     const logoutUser = useCallback(() =>{
         localStorage.removeItem("User");
         setUser(null);
@@ -56,7 +95,12 @@ export const AuthContextProvider = ({children}) => {
         registerUser,
         registerError,
         setIsRegisterLoading,
-        logoutUser
+        logoutUser,
+        loginUser,
+        loginError,
+        loginInfo,
+        updateloginInfo,
+        isloginLoading
     }}
         >
             {children}
--- Chat_Room/client/src/pages/Login.jsx
+++ Chat_Room/client/src/pages/Login.jsx
@@ -1,8 +1,19 @@
+import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from '../context/AuthContext';
 
-const Login = () => {
-    return<>
-    <Form>
+const Login = () => { 
+    const {
+        loginUser,
+        loginError,
+        loginInfo,
+        updateloginInfo,
+        isloginLoading
+    }  = useContext(AuthContext)
+
+    return(
+        <>
+    <Form onSubmit={loginUser}>
         <Row style={{
             height: "100vh",
             justifyContent: "center",
@@ -11,17 +22,30 @@ const Login = () => {
             <col xs={6}>
             <Stack gap={3}>
                 <h2>Login</h2>
-                <Form.Control type="email" placeholder="Email"/>
-                <Form.Control type="password" placeholder="Password"/>
+                <Form.Control type="email" placeholder="Email"
+                onChange={(e) => updateloginInfo({...loginInfo,
+                email:e.target.value})
+                }
+                />
+
+                <Form.Control type="password" placeholder="Password"
+                onChange={(e) => updateloginInfo({...loginInfo,
+                email:e.target.value})
+                }
+                />
                 <Button variant="primary" type="submit">
                     Login
                 </Button>
-                <Alert variant="danger"><p>Error Occured</p></Alert>
+                { loginError?.error &&
+                 <Alert variant="danger">
+                    <p>{loginError?.message}</p>
+                    </Alert>
+                    }
             </Stack>
             </col>
         </Row>
     </Form>
-    </>;
-};
+    </>
+)}
 
 export default Login;

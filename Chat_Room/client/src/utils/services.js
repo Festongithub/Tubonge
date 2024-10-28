8ce66e7af41d51dfe9e7becb6c64c502af933daa
Festongithub <fkemboik@gmail.com>
2024-10-28 12:09:49 +0300

    chat context

--- Chat_Room/client/src/utils/services.js
+++ Chat_Room/client/src/utils/services.js
@@ -1,4 +1,4 @@
-import { application } from "express"
+import { application, response } from "express"
 
 export const baseUrl = 'http://localhost:5000/api'
 
@@ -22,6 +22,22 @@ export const postRequest = async(url, body) => {
             message = data;
         }
         return{error: true, message};
+    }
+    return data;
+}
+
+export const getRequest = async(url) => {
+    const request = await fetch(url);
+
+    const data = await response.json()
+
+    if(!response.ok) {
+        let message = "An error occured"
+
+        if (data?.message){
+            message = data.message
+        }
+        return {error: true, message}
     }
     return data;
 }

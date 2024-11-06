0a7eb7a03faa7fe6f0d0c45a49831214b61c9041
Festongithub <fkemboik@gmail.com>
2024-11-06 12:51:46 +0300

    join room

--- Chat_Room/server/controllers/roomControllers.js
+++ Chat_Room/server/controllers/roomControllers.js
@@ -1,5 +1,7 @@
 const Room = require('../models/userRooms');
 const { v4 : uuidv4 } = require('uuid');
+const User = require('../models/user.model');
+
 
 // create a new Room
 const createRoom = async(req, res) => {
@@ -26,6 +28,31 @@ const createRoom = async(req, res) => {
         res.status(200).json({messge: "Room created successfully", room: newRoom });
     } catch(error){
         res.status(500).json({error: "server error"})
+    }
+}
+
+// Join A room
+const joinRoom = async(req, res) =>{
+    const { roomId, userId } = req.body;
+
+    try{
+        const room = await Room.findOne({roomId});
+
+        if(!room){
+            return res.status(404).json({error: "Room not found"});
+        }
+
+        if(room.members.includes(userId)){
+            return res.status(400).json({
+                error: "User is in the room already find another one"
+            })
+
+        // add new user to the members array
+        room.members.push(userId);
+        await room.save();
+        } catch(error){
+            
+        }
     }
 }
 
